f3404a42be4e120ec1f3f6cf6a818183679af30e
Martin Rios <28807563+martinrios95@users.noreply.github.com>
2022-07-04 17:38:33 -0300

    Exception Middleware en WebAPI

---
+++ ContactsAPI/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace ContactsAPI.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            } catch (Exception error)
+            {
+                await HandleExceptionAsync(context, error);
+            }
+        }
+
+        public async Task HandleExceptionAsync(HttpContext context, Exception error)
+        {
+            // Generate custom response with HTTP 503 error code
+        }
+    }
+}

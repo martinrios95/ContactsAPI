cdd1aa3fe5d9f60a5c7c2b460ecf62e149ae8d28
Martin Rios <28807563+martinrios95@users.noreply.github.com>
2022-07-05 17:18:00 -0300

    Agregar opciones de logging

--- ContactsAPI/Program.cs
+++ ContactsAPI/Program.cs
@@ -5,13 +5,20 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add console debugging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Switch to a SQLite database
 builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Set exception filter
 builder.Services.AddControllers(options => options.Filters.Add(new ExceptionFilter()));
 
 var app = builder.Build();
